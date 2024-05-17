87b623697b7f94aa16ad193a413739827304f03c
Mukul dubey <mdubey.0433@gmail.com>
2024-05-17 11:11:59 +0530

    added default date option and basic layout in calendar

--- app/routes/calendar.tsx
+++ app/routes/calendar.tsx
@@ -56,12 +56,10 @@ export default function Calendar() {
   }
   return (
     <div className="pt-5 h-screen overflow-hidden relative">
-      <div className="flex flex-col gap-10 justify-start items-center mx-5 h-full">
-        <div className="flex flex-col gap-6 mt-10 ">
+      <div className="flex flex-col gap-5 md:gap-10 justify-start items-center mx-5 h-full">
+        <div className="flex flex-col gap-3  mt-10 ">
           <span className="text-center font-bold text-[20px] select-none lg:select-auto">Create Priority Task</span>
           <Form className="flex flex-col gap-4" method="POST" action="/calendar">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <div className="grid gap-6 mb-6 md:grid-cols-2"></div>
             <div>
               <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                 Task Name
@@ -98,7 +96,7 @@ export default function Calendar() {
                 id="start"
                 name="dead-date"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="19/05/2024"
+                defaultValue={new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toISOString().split("T")[0]}
                 required
               />
             </div>
@@ -123,7 +121,7 @@ export default function Calendar() {
             </button>
           </Form>
         </div>
-        <div className="flex flex-col gap-6 mt-10 ">
+        <div className="flex flex-col gap-3 mt-10 ">
           <span className="text-center font-bold text-[20px] select-none lg:select-auto">Create Daily Task</span>
           <Form className="flex flex-col gap-4" method="POST" action="/calendar">
             <div>
